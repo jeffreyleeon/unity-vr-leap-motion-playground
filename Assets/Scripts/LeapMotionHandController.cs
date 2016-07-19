125bd06abc901f783dc119a8e5bfa93601c56479
Lee Chun On <jeffreyleeon@gmail.com>
2016-07-19 20:30:11 +0800

    First commit

---
+++ Assets/Scripts/LeapMotionHandController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class LeapMotionHandController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Controller controller = new Controller ();
+		Frame frame = controller.Frame();
+		if(frame.Hands.Count > 0) {
+			HandList hands = frame.Hands;
+			Hand firstHand = hands [0];
+			Debug.Log ("Hand id: " + firstHand.Id);
+		}
+	}
+
+	void OnHandFound( Hand h )
+	{
+		Debug.Log ("Hand found");
+	}
+}
